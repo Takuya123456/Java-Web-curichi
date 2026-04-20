1e8703924213ac6aeac7371ec75348948bf8c886
Takuya123456 <tacuyacanvara@gmail.com>
2026-04-19 19:14:52 -0500

    Delete backend directory

--- backend/src/main/java/com/senati/curichazo/repository/FiadoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.senati.curichazo.repository;
-import com.senati.curichazo.entity.Fiado;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface FiadoRepository extends JpaRepository<Fiado, Long> {}
--- backend/src/main/java/com/senati/curichazo/repository/VentaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.senati.curichazo.repository;
-import com.senati.curichazo.entity.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Long> {}
